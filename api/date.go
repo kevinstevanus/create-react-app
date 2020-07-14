cd328933406f14556e501ccb209885578ea9cdfa
Kevin Stevanus <kevinstevanus777@gmail.com>
2020-07-14 13:42:05 +0700

    time change

--- api/date.go
+++ api/date.go
@@ -3,11 +3,11 @@ package handler
 import (
 	"fmt"
 	"net/http"
-	"time"
+	// "time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
+	//currentTime := time.Now().Format(time.RFC850)
 	// fmt.Fprintf(w, currentTime)
 	fmt.Fprintf(w, "test")
 }
